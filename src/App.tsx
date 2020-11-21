48bcf9a72ea32e9bb2fa24765b1e1f5284cff455
RoyCrivolotti <roy.crivolotti@southworks.com>
2020-11-21 16:54:44 -0300

    Linting

--- src/App.tsx
+++ src/App.tsx
@@ -2,25 +2,28 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import SwaggerEditor, { plugins } from 'swagger-editor';
+import 'swagger-editor/dist/swagger-editor.css';
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className='App'>
+			<header className='App-header'>
+				<img src={logo} className='App-logo' alt='logo' />
+				<p>
+					Edit <code>src/App.tsx</code> and save to reload.
+				</p>
+				<a
+					className='App-link'
+					href='https://reactjs.org'
+					target='_blank'
+					rel='noopener noreferrer'
+				>
+					Learn React
+				</a>
+			</header>
+		</div>
+	);
 }
 
 export default App;
