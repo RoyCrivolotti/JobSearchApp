fea5d5419bee46138ab43b5e79b50bca58f717ca
RoyCrivolotti <roy.crivolotti@southworks.com>
2020-11-22 13:22:03 -0300

    Added empty func

--- Backend/src/controllers/SubscriptionController.ts
+++ Backend/src/controllers/SubscriptionController.ts
@@ -1,4 +1,9 @@
 export class SubscriptionController {
+    public async listSubscriptions(): Promise<void> {
+        await fetch('google.com');
+        throw new Error('Method not implemented.');
+    }
+
     public async checkOut(): Promise<void> {
         await fetch('google.com');
         throw new Error('Method not implemented.');
