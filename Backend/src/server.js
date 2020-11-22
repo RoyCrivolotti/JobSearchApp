d28f0e652369f02c05a5126bef85ad4a8e41cf02
RoyCrivolotti <roy.crivolotti@southworks.com>
2020-11-22 19:00:34 -0300

    Uncommented

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -17,51 +17,47 @@ const { developmentErrors, productionErrors, notFound } = require('./handlers/er
 
 dotenv.config();
 
-const firebaseBackup = require('../backup/migration');
+const app = express();
 
-firebaseBackup();
+const PORT = process.env.NODE_ENV ? process.env.NODE_ENV : '8080';
 
-// const app = express();
+// log only 4xx and 5xx responses to console
+app.use(logger('dev', {
+	skip: (req, res) => res.statusCode < 400,
+}));
 
-// const PORT = process.env.NODE_ENV ? process.env.NODE_ENV : '8080';
+// log all requests to access.log
+app.use(logger('common', {
+	stream: fs.createWriteStream(path.join(__dirname, 'access.log'), { flags: 'a' }),
+}));
 
-// // log only 4xx and 5xx responses to console
-// app.use(logger('dev', {
-// 	skip: (req, res) => res.statusCode < 400,
-// }));
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
 
-// // log all requests to access.log
-// app.use(logger('common', {
-// 	stream: fs.createWriteStream(path.join(__dirname, 'access.log'), { flags: 'a' }),
-// }));
+app.use(express.static(path.join(__dirname, 'build')));
 
-// app.use(cors());
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: false }));
-// app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-// app.use(express.static(path.join(__dirname, 'build')));
+// route handling
+app.use('/api', routes);
+app.get('/*', (req, res, next) => res.sendFile(path.join(__dirname, 'build', 'index.html'), error => {
+	if (error) next(error);
+}));
 
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({ extended: true }));
+// if the routes above didn't catch it, we 404 it and forward it to the error handler
+app.use(notFound);
 
-// // route handling
-// app.use('/api', routes);
-// app.get('/*', (req, res, next) => res.sendFile(path.join(__dirname, 'build', 'index.html'), error => {
-// 	if (error) next(error);
-// }));
+// otherwise, forward the error to the corresponding error handler given the environment
+// eslint-disable-next-line no-unused-expressions
+app.get('env') === 'development'
+	? app.use(developmentErrors)
+	: app.use(productionErrors);
 
-// // if the routes above didn't catch it, we 404 it and forward it to the error handler
-// app.use(notFound);
+app.listen(PORT, () => {
+	console.log(`Listening on port ${PORT}`);
+}).on('error', error => console.error(`Failed to listen to port ${PORT}`, error));
 
-// // otherwise, forward the error to the corresponding error handler given the environment
-// // eslint-disable-next-line no-unused-expressions
-// app.get('env') === 'development'
-// 	? app.use(developmentErrors)
-// 	: app.use(productionErrors);
-
-// app.listen(PORT, () => {
-// 	console.log(`Listening on port ${PORT}`);
-// }).on('error', error => console.error(`Failed to listen to port ${PORT}`, error));
-
-// module.exports = app;
+module.exports = app;
