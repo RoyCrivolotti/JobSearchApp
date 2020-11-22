5068e57f711c6abfeab5e66bd4bec15ffeef3767
RoyCrivolotti <roy.crivolotti@southworks.com>
2020-11-22 18:04:54 -0300

    Quick fix for ts/js

--- Backend/src/handlers/errorHandlers.ts
+++ Backend/src/handlers/errorHandlers.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
 /* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable @typescript-eslint/explicit-module-boundary-types */
 /* eslint-disable @typescript-eslint/no-unsafe-return */
@@ -9,6 +10,7 @@ import * as createError from 'http-errors';
 
 // this function is an error handle to deal with async controller methods
 // simply wrap the function as follows: catchError(asyncMethod)
+// @ts-ignore
 export const catchErrors = fn => function(req, res, next) {
     return fn(req, res, next).catch(next);
 };
@@ -16,12 +18,14 @@ export const catchErrors = fn => function(req, res, next) {
 // express interprets any route callback with four parameters as error-handling middleware
 // since next(error) stops handling the request and falls back on the error handling, notFound() is only called
 // if no error is thrown and no route matches the requested one
+// @ts-ignore
 export const notFound = (req, res, next) => {
     next(createError(404));
 };
 
 // if the route was indeed found and something broke, this is the fallback
 // detailed error object sent back in development
+// @ts-ignore
 export const developmentErrors = (error, req, res, next) => {
     const status =
         error && error.status
@@ -38,6 +42,7 @@ export const developmentErrors = (error, req, res, next) => {
 };
 
 // no stack traces are sent in production to avoid leaks
+// @ts-ignore
 export const productionErrors = (error, req, res, next) => {
     const status =
         error && error.status
