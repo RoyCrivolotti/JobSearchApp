845e66de8184e3100f62b8cb1e0756327cf8e4fb
RoyCrivolotti <roy.crivolotti@southworks.com>
2020-11-22 17:55:04 -0300

    Migrations is vanilla js

--- Backend/backup/migration.js
+++ Backend/backup/migration.js
@@ -1,15 +1,14 @@
 // In your index.js
 
-const firestoreService = require('firestore-export-import');
-const serviceAccount = require('./serviceAccountKey.json');
+import { initializeApp, backups } from 'firestore-export-import';
+import serviceAccount from './serviceAccountKey.json';
 
 // Initiate Firebase App
 // appName is optional, you can omit it.
-firestoreService.initializeApp(serviceAccount, databaseURL);
+initializeApp(serviceAccount, databaseURL);
 
 // Start exporting your data
-firestoreService
-    .backups(['services'])
+void backups(['services'])
     .then(collections => {
         console.log(JSON.stringify(collections));
     });
