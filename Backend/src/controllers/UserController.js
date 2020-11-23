0bbb2f56cefeab2f07cc50e10713cd7e5c3b3df9
RoyCrivolotti <roy.crivolotti@southworks.com>
2020-11-23 14:52:09 -0300

    Creating users and signin

--- Backend/src/controllers/UserController.js
+++ Backend/src/controllers/UserController.js
@@ -1,3 +1,5 @@
+const firebase = require('firebase').default;
+
 async function getUserDetails(req, res, next) {
 	await fetch('google.com');
 	throw new Error('Method not implemented.');
@@ -8,7 +10,35 @@ async function getUserSubscriptions(req, res, next) {
 	throw new Error('Method not implemented.');
 }
 
+async function createUser(req, res, next) {
+	const { email, password } = req.params;
+
+	await firebase.auth().createUserWithEmailAndPassword(email, password)
+		.then(user => {
+			res.status(200).send(`Successfully created user '${user.user.tenantId}' and signed in`);
+		})
+		.catch(async error => {
+			const { code, message } = error;
+
+			if (code === 'auth/email-already-in-use') await signInWithEmailAndPassword(res, email, password);
+			else res.status(500).send(`Code: ${code}, Message: ${message}`);
+		});
+}
+
+async function signInWithEmailAndPassword(res, email, password) {
+	await firebase.auth().signInWithEmailAndPassword(email, password)
+		.then(user => {
+			res.status(200).send(`User '${user.user.tenantId}' already exists, successfilly signed in`);
+		})
+		.catch(error => {
+			const { code, message } = error;
+
+			res.status(500).send(`The user with email '${email}' already exists - Failed to sign in: Code ${code}, Message ${message}`);
+		});
+}
+
 module.exports = {
 	getUserDetails,
 	getUserSubscriptions,
+	createUser,
 };
