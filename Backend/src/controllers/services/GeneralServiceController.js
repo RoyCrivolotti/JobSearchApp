bc47b4de3223f37ee2d17f146716e3345dd109fa
RoyCrivolotti <roy.crivolotti@southworks.com>
2020-11-23 15:29:05 -0300

    Getting all services' data

--- Backend/src/controllers/services/GeneralServiceController.js
+++ Backend/src/controllers/services/GeneralServiceController.js
@@ -1,9 +1,26 @@
-const db = require('../../database/firebase').default;
+const db = require('../../database/firebase');
+
+const { firestoreDocToJson } = require('../../helpers');
 
 async function getAllServicesData(req, res, next) {
-	console.log(db.collection('services'));
-	res.send('here');
-	// throw new Error('Method not implemented.');
+	const services = db.collection('services');
+
+	try {
+		const doc = await services.get();
+
+		const data = firestoreDocToJson(doc);
+
+		res.json(data);
+	} catch (error) {
+		let msg = '';
+
+		if (error && error.code === 'permission-denied') {
+			msg = `Code: ${error.code}, Origin: ${error.name}`;
+			console.error(msg);
+		}
+
+		res.status(403).send(msg || 'Unexpected error when trying to fetch data');
+	}
 }
 
 module.exports = {
